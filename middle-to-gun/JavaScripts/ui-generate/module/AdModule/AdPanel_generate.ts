474734296833ee16a8625f252e0c94173e8ceb89
HKZUNITY <3046916186@qq.com>
2024-05-20 00:33:37 +0800

    枪战校园都市(中路对狙)：替换ads

--- middle-to-gun/JavaScripts/ui-generate/module/AdModule/AdPanel_generate.ts
+++ middle-to-gun/JavaScripts/ui-generate/module/AdModule/AdPanel_generate.ts
@@ -3,7 +3,7 @@
  * WARNING: DO NOT MODIFY THIS FILE,MAY CAUSE CODE LOST.
  * AUTHOR: 爱玩游戏的小胖子
  * UI: UI/module/AdModule/AdPanel.ui
- * TIME: 2024.03.13-23.04.52
+ * TIME: 2024.05.20-00.20.34
  */
  
 @UIBind('UI/module/AdModule/AdPanel.ui')
@@ -29,10 +29,10 @@ export default class AdPanel_Generate extends UIScript {
 		}
 		return this.mNoBtn_Internal
 	}
-	private mYesBtn_Internal: mw.StaleButton
-	public get mYesBtn(): mw.StaleButton {
+	private mYesBtn_Internal: mw.AdsButton
+	public get mYesBtn(): mw.AdsButton {
 		if(!this.mYesBtn_Internal&&this.uiWidgetBase) {
-			this.mYesBtn_Internal = this.uiWidgetBase.findChildByPath('RootCanvas/Canvas/Canvas_1/mYesBtn') as mw.StaleButton
+			this.mYesBtn_Internal = this.uiWidgetBase.findChildByPath('RootCanvas/Canvas/Canvas_1/mYesBtn') as mw.AdsButton
 		}
 		return this.mYesBtn_Internal
 	}
@@ -52,13 +52,6 @@ export default class AdPanel_Generate extends UIScript {
 		});
 		this.initLanguage(this.mNoBtn);
 		this.mNoBtn.touchMethod = (mw.ButtonTouchMethod.PreciseTap);
-		
-	
-		this.mYesBtn.onClicked.add(()=>{
-			Event.dispatchToLocal("PlayButtonClick", "mYesBtn");
-		});
-		this.initLanguage(this.mYesBtn);
-		this.mYesBtn.touchMethod = (mw.ButtonTouchMethod.PreciseTap);
 		
 	
 		//按钮添加点击
