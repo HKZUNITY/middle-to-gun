4957b8fc7237bbd47be2e361ae5aa199f47240b9
HKZUNITY <3046916186@qq.com>
2024-05-20 00:33:37 +0800

    枪战校园都市(中路对狙)：替换ads

--- middle-to-gun/JavaScripts/module/AdModule/ui/AdPanel.ts
+++ middle-to-gun/JavaScripts/module/AdModule/ui/AdPanel.ts
@@ -1,4 +1,5 @@
-﻿import Utils from "../../../tools/Utils";
+﻿import { Notice } from "../../../common/notice/Notice";
+import Utils from "../../../tools/Utils";
 import AdPanel_Generate from "../../../ui-generate/module/AdModule/AdPanel_generate";
 
 export default class AdPanel extends AdPanel_Generate {
@@ -10,11 +11,15 @@ export default class AdPanel extends AdPanel_Generate {
     }
 
     private bindButtons(): void {
-        this.mYesBtn.onClicked.add(this.onClickYesButton.bind(this));
+        this.mYesBtn.onClose.add(this.onClickYesButton.bind(this));
         this.mNoBtn.onClicked.add(this.onClickNoButton.bind(this));
     }
 
-    private onClickYesButton(): void {
+    private onClickYesButton(isSuccess: boolean): void {
+        if (!isSuccess) {
+            Notice.showDownNotice(`${this.mYesBtn.text}失败，请重试`);
+            return;
+        }
         this.hideAdPanel();
         if (this.callback) this.callback();
     }
@@ -24,37 +29,12 @@ export default class AdPanel extends AdPanel_Generate {
     }
 
     private callback: () => void = null;
-    private yesText: string = "领取";
     public showRewardAd(callback: () => void, contentText: string, noText: string = "取消", yesText = "领取", isAuto: boolean = true): void {
         this.callback = callback;
         this.mContentTxt.text = contentText;
         this.mNoBtn.text = noText;
         this.mYesBtn.text = yesText;
-        this.yesText = yesText;
         this.showAdPanel();
-        if (isAuto) this.autoYes();
-    }
-
-    private yesInterval: any = null;
-    private autoYes(): void {
-        this.clearAutoYesInterval();
-        let time: number = 5;
-        this.mYesBtn.text = this.yesText + "(" + time + ")";
-        this.yesInterval = TimeUtil.setInterval(() => {
-            time--;
-            this.mYesBtn.text = this.yesText + "(" + time + ")";
-            if (time <= 0) {
-                this.onClickYesButton();
-                this.clearAutoYesInterval();
-            }
-        }, 1);
-    }
-
-    private clearAutoYesInterval(): void {
-        if (this.yesInterval) {
-            TimeUtil.clearInterval(this.yesInterval);
-            this.yesInterval = null;
-        }
     }
 
     public showAdPanel(): void {
@@ -63,7 +43,6 @@ export default class AdPanel extends AdPanel_Generate {
     }
 
     public hideAdPanel(): void {
-        this.clearAutoYesInterval();
         if (!this.visible) return;
         Utils.closeUITween(
             this.rootCanvas,
